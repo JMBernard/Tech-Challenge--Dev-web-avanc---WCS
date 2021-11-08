068362e3ed2690629f008d8600f78819102a7596
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-11-08 16:25:00 +0100

    input component css ok

--- src/components/PostInput.jsx
+++ src/components/PostInput.jsx
@@ -5,7 +5,14 @@ import './PostInput.css';
 function PostInput() {
     return (
         <div className="PostInput">
-            
+            <div className="input_content">
+                <h2 className="input_title">Ajouter un(e) Argonaute</h2>
+                <label className="input_label">Nom de l'argonaute</label>
+                <div className="input_field">
+                    <input type="text" placeholder="Charalampos"></input>
+                    <button>Envoyer</button>
+                </div>
+            </div>
         </div>
     )
 }
